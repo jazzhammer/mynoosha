676bce5b1dcb8054b5cea54e0e2a8c5492aa4fd0
jazzhammer <tio.a@tacteonltd.com>
2024-07-21 00:49:31 -0700

    feat(client): CR_D

--- back/api/test/test_clients.py
+++ back/api/test/test_clients.py
@@ -1,5 +1,22 @@
+import json
+
 import requests
 
-endpoint_get = 'http://localhost:8001/api/v0/clients/'
+endpoint_clients = 'http://localhost:8001/api/v0/clients/'
+TEST_NAME = 'asdf'
+
+
 def test_get():
-    requests.get(endpoint_get, params={})
+    # delete if exists
+    response = requests.get(endpoint_clients, params={'search': TEST_NAME})
+    if response.status_code == 200:
+        content = response.content.decode('utf8')
+        founds = json.loads(json.loads(content))
+        requests.delete(endpoint_clients, params={'id': founds[0]['id']})
+    response = requests.get(endpoint_clients, params={'search': TEST_NAME})
+    assert response.status_code != 200
+    # post
+    response = requests.post(endpoint_clients, json={'name': TEST_NAME})
+    assert response.status_code == 201
+    response = requests.get(endpoint_clients, params={'search': TEST_NAME})
+    assert response.status_code == 200
--- back/api/views.py
+++ back/api/views.py
@@ -1,12 +1,44 @@
 import json
 
+from django.forms import model_to_dict
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
+from .models import Client
+from .serializers import ClientSerializer
 
-@api_view(['GET', 'POST'])
+
+@api_view(['GET', 'POST', 'DELETE'])
 def clients(request):
     if request.method == 'GET':
+        if request.GET.get('search'):
+            search = request.GET.get('search')
+            founds = Client.objects.filter(name__contains=search)
+            if founds.exists():
+                return JsonResponse(json.dumps([model_to_dict(instance) for instance in founds]), status=200, safe=False)
+            else:
+                return JsonResponse(
+                    json.dumps({'detail':f'empty result for search={search}'}),
+                    status=404,
+                    safe=False
+                )
         return JsonResponse(json.dumps({'data': 'ok'}), status=200, safe=False)
+    elif request.method == 'POST':
+        if not Client.objects.filter(name=request.GET.get('name')).exists():
+            serializer = ClientSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                created = serializer.save()
+                return JsonResponse(model_to_dict(created), status=201, safe=False)
+            else:
+                return JsonResponse({'error': 'invalid data for Client'}, status=400, safe=False)
+        else:
+            return JsonResponse({'error': 'Client already exists'}, status=400, safe=False)
+    elif request.method == 'DELETE':
+        id = request.GET.get('id')
+        if Client.objects.filter(id=id).exists():
+            Client.objects.filter(id=id).delete()
+            return JsonResponse({}, status=200, safe=False)
+        else:
+            return JsonResponse({'error': f'not found to delete: {id=}'}, status=404, safe=False)
     else:
-        return JsonResponse(json.dumps({'data': 'POST unsupported'}), status=200, safe=False)
+        return JsonResponse(json.dumps({'data': f"{request.method} unsupported"}), status=400, safe=False)
