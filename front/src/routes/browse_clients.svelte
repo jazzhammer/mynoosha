a8264f1801a7a9a6539d297ced95d19a48a5063c
jazzhammer <tio.a@tacteonltd.com>
2024-07-26 04:14:34 -0700

    feat(Clients): add browse clients page

---
+++ front/src/routes/browse_clients.svelte
@@ -0,0 +1,23 @@
+<style>
+</style>
+<script lang="ts">
+  import ClientService from '../services/client.service';
+  import {type ClientCrud, ClientStore, crud} from "../stores";
+  import type {Client} from "../models/client";
+  import {onDestroy} from "svelte";
+
+  let client: Client;
+  $: client
+
+  const unsubClient = ClientStore.subscribe((ccrud: ClientCrud) => {
+    if (ccrud && ccrud.type === crud.READ) {
+      client = ccrud.payload as Client
+    }
+  });
+  onDestroy(unsubClient);
+
+</script>
+<div class="flex flex-col border-myroon-100 border p-3 ml-3 rounded w-4/12 text-myhigh_white"
+     style="min-width: 226px; max-width: 300px; font-size: 10pt;" data-testid="browse_clients">
+  browse
+</div>
--- front/src/routes/clients.svelte
+++ front/src/routes/clients.svelte
@@ -5,7 +5,9 @@
 </style>
 <script lang="ts">
   import NewClient from './new_client.svelte';
-  let mode = 'new';
+  import EditClient from './edit_client.svelte';
+  import BrowseClients from './browse_clients.svelte';
+  let mode = 'browse';
   $: mode
 
   function setMode(next: string): void {
@@ -14,6 +16,15 @@
 </script>
 <div class="">
   <div class="flex flex-row ml-3 mb-3 w-2/12">
+    {#if mode!=='browse'}
+      <div on:click={() => {setMode('browse')}}
+           on:keyup={() => {}}
+           tabindex="0"
+           role="button"
+           class="menu-item border border-t-1 rounded-md border-l-1 border-r-1 border-myblue-500 cursor-pointer hover:border-myblue-500 pl-6 pr-6">
+        browse
+      </div>
+    {/if}
     {#if mode!=='new'}
       <div on:click={() => {setMode('new')}}
            on:keyup={() => {}}
@@ -33,7 +44,13 @@
       </div>
     {/if}
   </div>
+  {#if mode === 'browse'}
+    <BrowseClients></BrowseClients>
+  {/if}
   {#if mode === 'new'}
     <NewClient></NewClient>
   {/if}
-</div>$
+  {#if mode === 'edit'}
+    <EditClient></EditClient>
+  {/if}
+</div>
