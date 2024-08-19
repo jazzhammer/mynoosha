db991b7f2a717b722e3b641a906c8046e7af5e40
jazzhammer <tio.a@tacteonltd.com>
2024-08-18 19:14:49 -0700

    feat(workInterval): assign to project

--- front/src/routes/project_work_intervals.svelte
+++ front/src/routes/project_work_intervals.svelte
@@ -6,11 +6,28 @@
 <script lang="ts">
   import type {Project} from "../models/project";
   import TimeRecorder from './time_recorder.svelte';
+  import WorkIntervalList from './work_interval_list.svelte';
+  import WorkIntervalService from "../services/work_interval.service";
+  import type {WorkInterval} from "../models/work_interval";
 
   export let project: Project;
   $: project
 
+  let workIntervals: WorkInterval[];
+  $: workIntervals
+
+  const getWorkIntervals = (): void => {
+    if (project) {
+      WorkIntervalService.find({project: project.id}).then((response: any) => {
+        workIntervals = response.data;
+
+      });
+    }
+  }
+  const selectWorkInterval = (next: WorkInterval): void => {
+    console.log(`project work intervals: selectedWorkInterval: ${next.id}`);
+  }
 </script>
 <div class="project-work-intervals">
-  <TimeRecorder></TimeRecorder>
+  <WorkIntervalList workIntervals={workIntervals} selectWorkInterval={selectWorkInterval}></WorkIntervalList>
 </div>
